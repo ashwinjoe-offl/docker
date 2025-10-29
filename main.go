958c368b659f29f92520cea5b48d2f3b40819fc3
Your Name <you@example.com>
2025-10-29 16:28:33 +0530

    feat: add timestamp to verify auto-deployment

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Hello from CI/CD test!")
+		fmt.Fprintf(writer, "Hello! This change was deployed by CI/CD at %s", time.Now().Format(time.RFC3339))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
