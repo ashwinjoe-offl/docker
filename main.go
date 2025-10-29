3b45d6c0f72f3b94132dd4203471087a074325b3
Your Name <you@example.com>
2025-10-29 17:08:16 +0530

    Update message to v3.0 to test CI/CD pipeline

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "🎯 CI/CD Test v2.0 - If you see this, the pipeline worked! Deployed at: %s", time.Now().Format(time.RFC3339))
+		fmt.Fprintf(writer, "� PIPELINE SUCCESS! v3.0 - This message was deployed through CI/CD at: %s", time.Now().Format(time.RFC3339))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
