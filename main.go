17837fd6d3705ebbd05b38c613f33a684d194803
Your Name <you@example.com>
2025-10-29 16:40:42 +0530

    Update application message to verify CI/CD pipeline

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello! This change was deployed by CI/CD at %s", time.Now().Format(time.RFC3339))
+		fmt.Fprintf(writer, "🚀 New Version: Automatically deployed via CI/CD at %s", time.Now().Format(time.RFC3339))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
